fd924ace31d57e59a4a75e4d60d068f85281679e
Eddie Kao <eddie@digik.com.tw>
2019-08-19 16:40:34 +0800

    finish add candidate feature and index and show

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,10 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
+  end
+
+  def show
+    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def new
@@ -8,11 +13,16 @@ class CandidatesController < ApplicationController
 
   def create
     # 寫入資料庫
-    @candidate = Candidate.new(params["candidate"])
+    # Strong Parameters
+    clean_params = params.require(:candidate).permit(:name, :age, :policy, :party)
+    @candidate = Candidate.new(clean_params)
+
     if @candidate.save
-      render html: 'ok'
+      flash[:notice] = "新增候選人成功!"
+      redirect_to root_path
     else
-      render html: 'fail'
+      # redirect_to new_candidate_path
+      render :new
     end
   end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,4 @@
 class Candidate < ApplicationRecord
+  validates :name, presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 40 }
 end
