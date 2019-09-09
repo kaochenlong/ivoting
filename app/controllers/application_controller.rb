637b8a3b79fb149d1f9b10b08a06b94e9adb8c6c
Eddie Kao <eddie@5xruby.tw>
2019-09-09 16:42:01 +0800

    add current_cart helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  helper_method :current_cart
+
   private
   def record_not_found
     render file: "#{Rails.root}/public/404.html",
            layout: false,
            status: :not_found
+  end
+
+  def current_cart
+    @cart = @cart || Cart.from_hash(session[:cart9527])
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-
   before_action :find_product, only: [:show, :add_to_cart]
 
   def index
@@ -10,16 +9,13 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    cart = Cart.from_hash(session[:cart9527])
-    cart.add_item(@product.id)
-
-    session[:cart9527] = cart.serialize
+    current_cart.add_item(@product.id)
+    session[:cart9527] = current_cart.serialize
 
     redirect_to products_path, notice: '已加入購物車'
   end
 
   private
-
   def find_product
     @product = Product.find(params[:id])
   end
