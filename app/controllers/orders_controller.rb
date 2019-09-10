5a2ee5d1406b7315f6bd5607e4e5cbf817091f8a
Eddie Kao <eddie@5xruby.tw>
2019-09-10 15:34:05 +0800

    add order_items to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,12 @@ class OrdersController < ApplicationController
   def create
     @order = current_user.orders.build(order_params)
 
+    current_cart.items.each do |item|
+      @order.order_items << OrderItem.new(product: item.product, quantity: item.quantity)
+    end
+
     if @order.save
+      session[:cart9527] = nil
       redirect_to payment_order_path(@order), notice: '訂單已成立'
     else
       render 'carts/checkout'
